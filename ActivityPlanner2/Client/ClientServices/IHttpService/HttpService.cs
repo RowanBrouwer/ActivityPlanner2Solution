61c751a2d430ae0be33444b944331daf0382369e
Rowan Brouwer <Rowan.Brouwer@itvitaelearning.nl>
2021-05-12 12:02:21 +0200

    Adjusted the Dispose

--- ActivityPlanner2/Client/ClientServices/IHttpService/HttpService.cs
+++ ActivityPlanner2/Client/ClientServices/IHttpService/HttpService.cs
@@ -58,34 +58,9 @@ namespace ActivityPlanner2.Client.ClientServices
             return result;
         }
 
-        protected virtual void Dispose(bool disposing)
-        {
-            if (!disposedValue)
-            {
-                if (disposing)
-                {
-                    // TODO: dispose managed state (managed objects)
-                    Http.Dispose();
-                }
-
-                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
-                // TODO: set large fields to null
-                disposedValue = true;
-            }
-        }
-
-        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
-        ~HttpService()
-        {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: false);
-        }
-
         public void Dispose()
         {
-            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
-            Dispose(disposing: true);
-            GC.SuppressFinalize(this);
+            ((IDisposable)Http).Dispose();
         }
     }
 }
